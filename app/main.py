724e8e4d5e7e7219b51a1e6f76abddcb7eda27fb
keurcien <keurcien.luu@gmail.com>
2021-10-29 01:42:03 +0200

    Mirror stretch

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import base64
 import io
-
+import requests
 import cv2 as cv
 import numpy as np
 from fastapi import FastAPI, Form, HTTPException
@@ -16,44 +16,86 @@ app.add_middleware(
 )
 
 
-def stretch_horizontally(img, parameter=50):
+class Squarimage:
 
-    height, width, _ = img.shape
+    def __init__(self, img):
+        self.img = img
+        self.height, self.width, _ = img.shape
 
-    if height > width:
-        padding = (height - width) // 2
-        blur_radius = 10
-        right = parameter
-        left = parameter
-        # right = int(0.01 * width)
-        # left = int(0.01 * width)
+    def stretch(self, left=0, right=0, mirror=False):
+        if self.height > self.width:
 
-        left_part = img[:, 0:left, :]
-        left_width = padding + left
+            if mirror:
+                return Image.fromarray(self._reflect(left, right))
 
-        right_part = img[:, -right:, :]
-        right_width = height - width - padding + right
+            if left > 0 and right == 0:
+                return Image.fromarray(self._stretch_left(left))
 
-        center_part = img[:, left:-right:, :]
+            if left == 0 and right > 0:
+                return Image.fromarray(self._stretch_right(right))
 
-        left_img = cv.resize(
-            left_part, (left_width, height), interpolation=cv.INTER_AREA
-        )
-        right_img = cv.resize(
-            right_part, (right_width, height), interpolation=cv.INTER_AREA
-        )
+            if left > 0 and right > 0:
+                return Image.fromarray(self._stretch_both_sides(
+                    left=left, right=right))
+        else:
+            return Image.fromarray(self.img)
 
-        res = np.concatenate((left_img, center_part, right_img), axis=1)
-        res[:, padding : (padding + 2 * left), :] = cv.blur(
-            res[:, padding : (padding + 2 * left), :], (blur_radius, blur_radius)
-        )
-        res[:, -(right_width + right) : -(right_width - right), :] = cv.blur(
-            res[:, -(right_width + right) : -(right_width - right), :],
-            (blur_radius, blur_radius),
-        )
-        return Image.fromarray(res)
-    else:
-        return Image.fromarray(img)
+    def _stretch_left(self, left):
+        padding = self.height - self.width
+
+        left_side = self.img[:, 0:left, :]
+        left_side_width = padding + left
+
+        right_side = self.img[:, left:, :]
+
+        left_side_resized = cv.resize(
+            left_side, (left_side_width, self.height), interpolation=cv.INTER_AREA)
+        resized_img = np.concatenate((left_side_resized, right_side), axis=1)
+        return resized_img
+
+    def _stretch_right(self, right):
+        padding = self.height - self.width
+
+        right_side = self.img[:, -right:, :]
+        right_side_width = padding + right
+
+        left_side = self.img[:, 0:-right, :]
+
+        right_side_resized = cv.resize(
+            right_side, (right_side_width, self.height), interpolation=cv.INTER_AREA)
+        resized_img = np.concatenate((left_side, right_side_resized), axis=1)
+
+        return resized_img
+
+    def _stretch_both_sides(self, left, right):
+
+        padding = (self.height - self.width) // 2
+
+        left_side = self.img[:, 0:left, :]
+        left_side_width = padding + left
+
+        right_side = self.img[:, -right:, :]
+        right_side_width = self.height - self.width - padding + right
+
+        center = self.img[:, left:-right:, :]
+
+        left_side_resized = cv.resize(
+            left_side, (left_side_width, self.height), interpolation=cv.INTER_AREA)
+        right_side_resized = cv.resize(
+            right_side, (right_side_width, self.height), interpolation=cv.INTER_AREA)
+
+        resized_img = np.concatenate(
+            (left_side_resized, center, right_side_resized), axis=1)
+        return resized_img
+
+    def _reflect(self, left, right):
+        if left > 0 and right > 0:
+            padding = (self.height - self.width) // 2
+            return cv.copyMakeBorder(self.img, 0, 0, padding, self.height - self.width - padding, cv.BORDER_REFLECT)
+        elif left > 0 and right == 0:
+            return cv.copyMakeBorder(self.img, 0, 0, self.height - self.width, 0, cv.BORDER_REFLECT)
+        elif left == 0 and right > 0:
+            return cv.copyMakeBorder(self.img, 0, 0, self.height - self.width, 0, cv.BORDER_REFLECT)
 
 
 @app.get("/")
@@ -62,19 +104,33 @@ def index():
 
 
 @app.post("/")
-def stretch(file: str = Form(...), name: str = Form(...), parameter: int = Form(...)):
+def stretch(file: str = Form(...), name: str = Form(...), left: int = Form(...), right: int = Form(...), mirror: bool = Form(...)):
     try:
-        format, imgstr = file.split(";base64,")
-        nparr = np.fromstring(base64.b64decode(imgstr), np.uint8)
-        img = cv.imdecode(nparr, cv.IMREAD_COLOR)
-        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
-        img_ = stretch_horizontally(img, parameter=parameter)
+        if not file.startswith("http"):
+            format, imgstr = file.split(";base64,")
+            nparr = np.fromstring(base64.b64decode(imgstr), np.uint8)
+            img = cv.imdecode(nparr, cv.IMREAD_COLOR)
+            img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+        else:
+            response = requests.get(file)
+            img = Image.open(io.BytesIO(response.content))
+            img = np.asarray(img)
+
+        squarimg = Squarimage(img)
+        img_ = squarimg.stretch(left=left, right=right, mirror=mirror)
 
         buff = io.BytesIO()
         img_.save(buff, format="JPEG")
         imgstr_ = base64.b64encode(buff.getvalue()).decode("utf-8")
+        return {
+            "name": name,
+            "dataURL": f"data:image/jpeg;base64,{imgstr_}"
+        }
 
-        return {"name": name, "dataURL": f"{format};base64,{imgstr_}"}
     except:
-        raise HTTPException(status_code=500, detail=f"Could not process {name}.")
+        raise HTTPException(
+            status_code=500,
+            detail=f"Could not process {name}."
+        )
